826911bb3bd3a4536b4b12f7eac9b9c3fdb2bb7f
datcongchua123 <hoaang2005@gmail.com>
2023-09-22 12:11:09 +0700

    Add project files.

---
+++ btapne/vitriptu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace baitane
+{
+    internal class vitriptu
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int dem = 0;
+            Console.Write("Nhap so luong phan tu :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu thu " + (i + 1) + ":");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+                if (max <= A[i])
+                {
+                    max = A[i];
+                    dem = i + 1;
+                }
+            }
+            Console.WriteLine("Cac phan tu mang :");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Phan tu " + (i + 1) + "la :" + A[i]);
+
+            }
+            Console.WriteLine("So lon nhat :" + max);
+            Console.WriteLine("Vi tri :" + dem);
+            Console.ReadKey();
+        }
+    }
+}
